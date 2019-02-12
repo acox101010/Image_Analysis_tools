73f9278a0bfcb824ada3629437a62bdc80b5a576
A.Cox <40004388+acox101010@users.noreply.github.com>
2019-02-12 16:04:32 -0500

    Update tempMatch_latest.py
    added preview feature

--- tempMatch_latest.py
+++ tempMatch_latest.py
@@ -1,13 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 14:10:34 2019
+@author: A8DPDZZ
+"""
 import glob
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-
-#fp_img = (r'C:\Users\Reaper124\Documents\Python Scripts\testimages') #filepath for all images
-#fp_img_gry = (r'C:\Users\Reaper124\Documents\Python Scripts\testimages\grey')
-#temp_cl = cv2.imread(r'C:\Users\Reaper124\Documents\Python Scripts\temp.png',0) #directory of image template
-#mydir = (r'M:\ETR Testing Files\ETR-X380N-4935')  # Replace mydir with the directory you want
 
 template = cv2.imread(r"C:\Users\A8DPDZZ\TestScripts\testfiles\temp2.jpg",0) #directory of template
 w, h = template.shape[::-1] #finds shape of template
@@ -25,10 +25,9 @@ for fil in glob.glob("*.jpg"):
             a = np.asarray(loc[0])[np.newaxis] #Transposes location matching coordinates
             detectedObjects = detectedObjects+1
             print(detectedObjects,"Template is a match in file:",fil)
-            #for pt in zip(*loc[::-1]): #used for locating the matched template in the image
-                #cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
-                #cv2.imshow("Image",image)
-                #cv2.waitKey(25)
+            for pt in zip(*loc[::-1]): #used for locating the matched template in the image
+                cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+                plt.imshow(image)
             with open("foo.csv",'a',newline='') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=' ')
                 spamwriter.writerow([fil, "_TempMatch"])
