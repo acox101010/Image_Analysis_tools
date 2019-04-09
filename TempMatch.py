79c0a5ed4d59b85a0fa1e3ecbf2a0249afd795d2
A.Cox <40004388+acox101010@users.noreply.github.com>
2019-04-09 15:47:55 -0400

    Add files via upload
    Added ipython notebook and updated original with data analysis

--- TempMatch.py
+++ TempMatch.py
@@ -2,6 +2,7 @@
 """
 Created on Mon Feb 11 14:10:34 2019
 @author: A8DPDZZ
+updated 4/9/2019 for data analysis
 """
 import glob
 import cv2
@@ -10,10 +11,11 @@ import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 
+#Load Template and draw boundary
 template = cv2.imread(r"M:\tempMatchTest\temp.jpg",0) #directory of template
 w, h = template.shape[::-1] #finds shape of template
 detectedObjects = 0 #variable for counting image iteration
-plt.imshow(template) #shows template
+#plt.imshow(template) #shows template
 
 #Writing initial row to csv for data processing purposes
 ofile  = open('match_results.csv', "w")
@@ -31,7 +33,7 @@ for fil in glob.glob("*.jpeg"):
         if np.any(loc[0]) > 0: 
             a = np.asarray(loc[0])[np.newaxis] #Transposes location matching coordinates
             detectedObjects = detectedObjects+1
-            print(detectedObjects,"Template is a match in file:",fil)
+            print(detectedObjects,"Template located in:",fil)
             
             for pt in zip(*loc[::-1]): #used for locating the matched template in the image
                 cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
@@ -39,25 +41,35 @@ for fil in glob.glob("*.jpeg"):
                 
             with open("match_results.csv",'a',newline='') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=' ')
-                spamwriter.writerow([fil, "_TempMatch"])
+                spamwriter.writerow(["Located"])
         else:
             detectedObjects = detectedObjects+1
-            print(detectedObjects,"Template not located in image:",fil)
+            print(detectedObjects,"Template not located in:",fil)
             
             with open("match_results.csv",'a',newline='') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=' ')
-                spamwriter.writerow([fil, "_NoMatch"])
+                spamwriter.writerow(["NotLocated"])
             continue
     except KeyboardInterrupt:
 
         cv2.destroyAllWindows()
         cv2.waitKey(0)
-        
-#load data set from csv into data frame
-df = pd.read_csv("match_results.csv")
-im_dat = df['Data'].values
-print("Dimensions of y before reshaping: {}".format(im_dat.shape))
-im_dat = im_dat.reshape(-1,1)
-print(df.describe())
-dat_bool = df.Data.str.contains('TempMatch')
-print(dat_bool)
+
+#Execute Data Analysis with results
+df = pd.read_csv('match_results.csv')
+missdat = df.isnull()
+missdat.head() #Count any missing rows?
+
+#search for any missing data and count values
+for column in missdat.columns.values.tolist():
+    print(column)
+    print (missdat[column].value_counts())
+    print("")
+    
+#generate count statistics
+for column in df.columns.values.tolist():
+    print(column)
+    print (df[column].value_counts())
+    print("")
+
+    
